5a5299dafb79e0c3d46c1f880a0d353ef1bc76ec
Ali <s202281300@kfupm.edu.sa>
2026-03-30 09:17:54 +0300

    done task 1-4

--- 6-1-back-end-fundamentals/backend/server.js
+++ 6-1-back-end-fundamentals/backend/server.js
@@ -1,13 +1,15 @@
 // TODO 1: Import Express
-
+import express from 'express';
 
 
 // TODO 2: Create the Express app and store it in a variable named app
-
+const app = express();
 
 
 // TODO 3: Allow React to access the server
 
+import cors from 'cors';
+app.use(cors());
 
 
 // TODO 5: Create the home route "/"
@@ -23,3 +25,6 @@
 
 
 // TODO 4: Start the server on port 3000
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
